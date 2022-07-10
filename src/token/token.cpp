4f4aa56e8eb823d9d3076bbb7ecbaf2ac661d95d
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-07-09 23:24:47 -0400

    implement keyword finder and to string functions for Token class

--- src/token/token.cpp
+++ src/token/token.cpp
@@ -1,4 +1,25 @@
 #include <string>
+#include <map>
 #include "Token.hpp"
 
+const std::string eof = "EOF";
+const std::string IDENT = "IDENT";
+const std::string INT = "INT";
+const std::string PLUS = "+";
+const std::string LPAREN = "(";
+const std::string RPAREN = ")";
+const std::string DEFINE = "define";
+const std::map<std::string, std::string> KEYWORDS = {{"define", DEFINE}};
+
 Token::Token(std::string type, std::string literal) : type_(type), literal_(literal) {}
+
+std::string Token::lookup_ident(std::string ident) { 
+    if (KEYWORDS.find(ident) != KEYWORDS.end()) { 
+        return KEYWORDS.find(ident)->second;
+    }
+    return IDENT;
+}
+
+std::string Token::to_string() { 
+    return "{Type:" + type_ + " Literal:" + literal_ + "}";
+}
--- src/token/token.hpp
+++ src/token/token.hpp
@@ -2,18 +2,21 @@
 #define TOKEN_H
 
 extern const std::string eof;
-extern cosnt std::string IDENT;
+extern const std::string IDENT;
 extern const std::string INT;
 extern const std::string PLUS;
 extern const std::string LPAREN;
 extern const std::string RPAREN;
 extern const std::string DEFINE;
+extern const std::map<std::string, std::string> KEYWORDS;
 
 class Token { 
     public:
         std::string type_;
         std::string literal_;
-        Token(std::string type, std::string literal); 
+        Token(std::string type, std::string literal);
+        std::string lookup_ident(std::string ident);
+        std::string to_string();
 };
 
 #endif  // TOKEN_H
