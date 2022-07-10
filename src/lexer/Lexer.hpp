6a5cedce5d016f8b0e47f6468f1b1686f4cd60e3
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-07-10 01:05:14 -0400

    I don't konw what changed lol

--- src/lexer/Lexer.hpp
+++ src/lexer/Lexer.hpp
@@ -3,16 +3,16 @@
 
 class Lexer { 
     public:
-        std::string input;
-        int position;
-        int read_position;
-        char ch;
+        std::string input_;
+        int position_;
+        int read_position_;
+        char ch_;
         Lexer(std::string input);
         void read_char();
         void eat_white_space();
         char peek_char();
-        boolean is_letter(char ch);
-        boolean is_digit(char ch);
+        bool is_letter(char ch);
+        bool is_digit(char ch);
         std::string read_identifier();
         std::string read_number();
         Token next_token();
