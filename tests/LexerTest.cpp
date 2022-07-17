60de85f60cf420e6ce8886cc59a4719aff215c40
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-07-16 22:03:24 -0400

    fix linker issues and complete lexer testsing

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-#include <src/lexer/Lexer.hpp>
-#include "src/token/Token.hpp"
+#include <vector>
+#include "../src/token/Token.hpp"
+#include "../src/lexer/Lexer.hpp"
 
 int main() { 
-    std::string input = "(define ten (+ 3 7)) (+ 1 8)";
-    Token tests[13];
+    std::string input = "(define ten (+ 3 7)) (+ 1 137)";
+    std::vector<Token> tests;
     Lexer l = Lexer(input);
-    
-    tests[0] = Token(LPAREN, "(");
-    tests[1] = Token(DEFINE, "define");
-    tests[2] = Token(IDENT, "ten");
-    tests[3] = Token(LPAREN, "(");
-    tests[4] = Token(PLUS, "+");
-    tests[5] = Token(INT, "3");
-    tests[6] = Token(INT, "7");
-    tests[7] = Token(RPAREN, ")");
-    tests[8] = Token(RPAREN, ")");
-    tests[9] = Token(LPAREN, "(");
-    tests[10] = Token(PLUS, "+");
-    tests[11] = Token(INT, "1");
-    tests[12] = Token(INT, "8");
-    tests[13] = Token(RPAREN, ")");
+
+    tests.push_back(Token(LPAREN, "("));
+    tests.push_back(Token(DEFINE, "define"));
+    tests.push_back(Token(IDENT, "ten"));
+    tests.push_back(Token(LPAREN, "("));
+    tests.push_back(Token(PLUS, "+"));
+    tests.push_back(Token(INT, "3"));
+    tests.push_back(Token(INT, "7"));
+    tests.push_back(Token(RPAREN, ")"));
+    tests.push_back(Token(RPAREN, ")"));
+    tests.push_back(Token(LPAREN, "("));
+    tests.push_back(Token(PLUS, "+"));
+    tests.push_back(Token(INT, "1"));
+    tests.push_back(Token(INT, "137"));
+    tests.push_back(Token(RPAREN, ")"));
 
     bool flag = true;
-    for (int i = 0; i < 13; ++i) { 
-        Token tt = tests[i];
+    for (int i = 0; i < tests.size(); ++i) { 
+        Token tt = tests.at(i);
         Token tok = l.next_token();
 
-        if (!tok.type_.compare(tt.type_)) {
-            //System.out.println("tests[" + i + "]" + "- tokentype wrong. expected=" + tt.type + ", got=" + tok.type);
-            std::cout << "tests[" << i << "]- token type wrong. expected=" << tt.type_ << ", got=" << tok.type_;
+        if (tok.type_.compare(tt.type_)) { //
+            std::cout << "tests[" << i << "] - token type wrong. expected=" << tt.type_ << ", got=" << tok.type_ << "\n";
             flag = false;
         }
 
-        if (!tok.literal_.compare(tt.literal_)) { 
-            //System.out.println("tests[" + i + "]" + "- literal wrong. expect=" + tt.literal + ", got=" + tok.literal);
-            std::cout << "tests[" << i << "]- literal wrong. expected=" << tt.literal_ << ", got=" << tok.literal_;
+        if (tok.literal_.compare(tt.literal_)) { //
+            std::cout << "tests[" << i << "] - literal wrong. expected=" << tt.literal_ << ", got=" << tok.literal_ << "\n";
             flag = false;
         }
     }
