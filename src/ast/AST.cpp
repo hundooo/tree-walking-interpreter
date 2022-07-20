d84595c93a4c05227def8cb69d592be45bd46f53
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-07-19 23:51:24 -0400

    create Node class constructors

--- src/ast/AST.cpp
+++ src/ast/AST.cpp
@@ -1,1 +1,6 @@
+#include <stdlib.h>
 #include "AST.hpp"
+
+Node::Node(int val) : left_(NULL), right_(NULL), val_(val), ident_(""), oper_("") {}
+
+Node::Node(std::string oper) : left_(NULL), right_(NULL), val_(0), ident_(""), oper_(oper) {}
