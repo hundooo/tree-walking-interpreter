29dd506b9fc16ff6041def229ca21f606707ed14
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-07-19 23:49:22 -0400

    create function to print AST for testing

--- tests/ParserTest.cpp
+++ tests/ParserTest.cpp
@@ -7,6 +7,26 @@
 #include "../src/ast/AST.hpp"
 #include "../src/parser/Parser.hpp"
 
+void printBT(const std::string& prefix, const Node* node, bool isLeft) {
+    if(node != NULL) {
+        std::cout << prefix;
+
+        std::cout << (isLeft ? "├──" : "└──" );
+
+        if (node->left_ == NULL && node->right_ == NULL) { 
+            std::cout << node->val_ << std::endl;
+        } else {
+            std::cout << node->oper_ << std::endl;
+        }
+
+        printBT(prefix + (isLeft ? "│   " : "    "), node->m_left, true);
+        printBT(prefix + (isLeft ? "│   " : "    "), node->m_right, false);
+    }
+}
+
+void printBT(const Node* node) {
+    printBT("", node, false);    
+}
 
 
 int main() { 
