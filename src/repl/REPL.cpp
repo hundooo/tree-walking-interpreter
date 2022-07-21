9349380ddd702932b72a996eb131cef754f1aef5
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-07-20 23:16:40 -0400

    create hashmap to store constant variables and function to add constants to hashmap

--- src/repl/REPL.cpp
+++ src/repl/REPL.cpp
@@ -15,7 +15,10 @@ void REPL::start() {
         std::string line;
         std::getline(std::cin, line);
 
-        if (!line.compare("")) { 
+        if (!line.compare("")) {
+            for (auto& it: constants) { 
+                delete it.second;
+            }
             break;
         }
 
@@ -24,15 +27,21 @@ void REPL::start() {
         std::vector<Node *> root = p.parse();
 
         for (int i = 0; i < root.size(); ++i) {
-            printf("%d\n", eval(root[i]));
-            p.node_destroy(root[i]);
+            if (root[i]->ident_.compare("") && root[i]->left_ != NULL) { 
+                add_constant(root[i]);
+            } else {
+                printf("%d\n", eval(root[i]));
+                p.node_destroy(root[i]);
+            }
         }
     }
 }
 
 int REPL::eval(Node *n) { 
-    if (n->left_ == NULL && n->right_ == NULL) { 
+    if (n->left_ == NULL && n->right_ == NULL && !n->ident_.compare("")) { 
         return n->val_;
+    } else if (n->ident_.compare("")) {
+        return eval(constants.find(n->ident_)->second);
     } else {
         if (n->oper_ == "+") { 
             return eval(n->left_) + eval(n->right_);
@@ -40,4 +49,11 @@ int REPL::eval(Node *n) {
             return -1;
         }
     }
+}
+
+void REPL::add_constant(Node *n) {
+    std::string ident = n->ident_;
+    Node *tmp = n->left_;
+    delete n; 
+    constants.insert({ident, tmp});
 }
--- src/repl/REPL.hpp
+++ src/repl/REPL.hpp
@@ -1,15 +1,18 @@
 #ifndef REPL_H
 #define REPL_H
 
+#include <unordered_map>
 #include <string>
 #include "../ast/AST.hpp"
 
 class REPL { 
     public: 
         const std::string PROMPT;
+        std::unordered_map<std::string, Node *> constants;
         REPL();
         void start();
         int eval(Node *n);
+        void add_constant(Node *n);
 };
 
 #endif  // REPL_H
